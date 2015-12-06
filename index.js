6b0a88750eab17401c7f5a147e90bebf0d734a46
kbrady <anne.kbrady@gmail.com>
2015-12-06 16:24:37 -0600

    Add page examples for all active links

--- index.js
+++ index.js
@@ -15,11 +15,23 @@ app.get('/', function(request, response) {
 app.get('/login', function(request, response) {
 	response.render('pages/login');
 });
+app.get('/sign_up', function(request, response) {
+	response.render('pages/sign_up');
+});
 app.get('/about', function(request, response) {
 	response.render('pages/about');
 });
 app.get('/faq', function(request, response) {
 	response.render('pages/faq');
+});
+app.get('/new_york', function(request, response) {
+	response.render('pages/place');
+});
+app.get('/nashville', function(request, response) {
+	response.render('pages/place');
+});
+app.get('/contact', function(request, response) {
+	response.render('pages/contact');
 });
 
 app.listen(app.get('port'), function() {
